8a27ea542bfac76ecce7499557cb524e40341fa4
julie <7jujubuilds@gmail.com>
2025-04-16 19:29:16 +0900

    컨트롤러의 뷰 반환 관련 학습
    - 결론: 컨트롤러(`@Controller`) 내 문자열을 반환하는 메서드는 templates 패키지에 해당 문자열에 해당하는 문서가 있거나 `@ResponseBody`가 있어야 함

--- spring-mvc-1/initial/src/main/java/cholog/MemberController.java
+++ spring-mvc-1/initial/src/main/java/cholog/MemberController.java
@@ -1,18 +1,48 @@
 package cholog;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class MemberController {
 
-    public String world() {
-        // TODO: /hello 요청 시 resources/templates/static.html 페이지가 응답할 수 있도록 설정하세요.
-        // TODO: 쿼리 파라미터로 name 요청이 들어왔을 때 해당 값을 hello.html에서 사용할 수 있도록 하세요.
-        return null;
+    // 얘는 Thymeleaf 없이는 사용할 수 없음--그리고 templates 패키지에 반환하는 문자열 이름을 가진 HTML 문서가 있어야 함
+    // 따라서 localhost:8080/a 로 가도 hi.html가 뜨지 않음
+    @GetMapping("/a")
+    public String pageA() {
+        return "hi";
     }
 
+    // 응답 값으로 페이지에 띄울 text/html Content-Type을 반환하는 메서드
+    // @ResponseBody annotation으로 인해 text/html Content-Type인 것으로 인지됨
+    // HTML syntax 사용 가능
+    @GetMapping("/b")
+    @ResponseBody
+    public String pageB() {
+        return "<p>Opens a page with this text as its <strong>only content</strong></p>";
+    }
+
+    // Thymeleaf 엔진은 파일 경로도 인식 가능
+    // localhost:8080/c 로 이동하면 static.html 페이지가 뜸
+    @GetMapping("/c")
+    public String pageC() {
+        return "./static.html";
+    }
+
+    // 템플릿 엔진을 사용해서 뷰 페이지를 클라이언트에 반환하는 메서드
+    @GetMapping("/hello")
+    public String world(@RequestParam(name = "name", required = false, defaultValue = "World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello";
+    }
+
+    // 클라이언트에 JSON 데이터를 반환하는 메서드
+    @GetMapping("/json")
+    @ResponseBody
     public Person json() {
-        // TODO: /json 요청 시 {"name": "brown", "age": 20} 데이터를 응답할 수 있도록 설정하세요.
-        return null;
+        return new Person("brown", 20);
     }
 }
